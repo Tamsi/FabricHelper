927c85c668c93c68891407490671fce9b91e2364
Tamsi <tamsi.besson@gmail.com>
2015-10-27 15:11:05 +0100

    recognize the activeObject without mouse's selection

--- js/FabricHelper.js
+++ js/FabricHelper.js
@@ -5,9 +5,7 @@ FabricHelper = function(canvasId, backgrounds, image) {
 	this.canvasId = canvasId;
 	this.bgIndex = 0;
 	this.position = [];
-
 	this.activeObject;
-
 
 	this.canvas = new fabric.Canvas(this.canvasId, {	
 	  selection: false
@@ -24,17 +22,36 @@ FabricHelper = function(canvasId, backgrounds, image) {
 	  this.canvas.add(img).setActiveObject(img);
 	}.bind(this));
 
-	this.listen('object:rotating');
-	this.listen('object:scaling');
-	this.listen('object:moving');
+	this.listenFabric('object:rotating');
+	this.listenFabric('object:scaling');
+	this.listenFabric('object:moving');
+	this.listenKeyboard();
+	this.listenDblClick();
+}
 
+FabricHelper.prototype.listenFabric = function(e) {
+	this.canvas.on(e, function(event) {
+	  if (event.target) {
+	  	this.activeObject = event.target;
+	    //this.position[this.bgIndex] = "(" + event.target.scaleX + "," + event.target.scaleY + "," + event.target.left + "," + event.target.top + "," + event.target.angle + ")";
+	    this.position[this.bgIndex] = this.setPosition();
+	  }
+	}.bind(this));
+}
 
+FabricHelper.prototype.listenDblClick = function(e) {
 	document.addEventListener("dblclick", function(e) {
 		console.log(this.position);
 	}.bind(this));
+}
 
+FabricHelper.prototype.listenKeyboard = function() {
 	document.addEventListener('keydown', function(e) {
 		this.activeObject = this.canvas.getActiveObject();
+		if (!this.activeObject) {
+			this.activeObject = this.canvas.item(0);
+			this.canvas.setActiveObject(this.canvas.item(0));
+		}
     	//left
 	    if(e.keyCode == 37) {
 	    	e.preventDefault();
@@ -63,13 +80,14 @@ FabricHelper = function(canvasId, backgrounds, image) {
 	            this.activeObject.top += 1;
         	}
 	    }
-	    this.activeObject.setCoords();
-	    this.canvas.renderAll();
-	}.bind(this));
 
-	document.addEventListener("keydown", function(e) {
-		if(e.keyCode === 68) {
+	    if(e.keyCode === 68) {
 			e.preventDefault();
+			//si mon tableau a l'index en cours est vide, je mets les coo
+			if(typeof this.position[this.bgIndex] == 'undefined'){
+				//this.position[this.bgIndex] = "(" + this.activeObject.scaleX + "," + this.activeObject.scaleY + "," + this.activeObject.left + "," + this.activeObject.top + "," + this.activeObject.angle + ")";
+				this.position[this.bgIndex] = this.setPosition();
+			}
 			if(this.bgIndex + 1 < this.backgrounds.length) {
 				this.bgIndex += 1;
 			} else {
@@ -78,23 +96,20 @@ FabricHelper = function(canvasId, backgrounds, image) {
 			this.canvas.setBackgroundImage(this.backgrounds[this.bgIndex], this.canvas.renderAll.bind(this.canvas));
 		} else if(e.keyCode === 81) {
 			e.preventDefault();
-			if(this.bgIndex - 1 > 0) {
+			if(this.bgIndex - 1 >= 0) {
 				this.bgIndex -= 1;
 			} else {
 				this.bgIndex = this.backgrounds.length - 1;
 			}
 			this.canvas.setBackgroundImage(this.backgrounds[this.bgIndex], this.canvas.renderAll.bind(this.canvas));
 		}
+
+	    this.activeObject.setCoords();
+	    this.canvas.renderAll();
 	}.bind(this));
+
 }
 
-FabricHelper.prototype.listen = function(e) {
-	this.canvas.on(e, function(object) {
-	  if (object.target) {
-	    this.position[this.bgIndex] = "(" + object.target.scaleX + "," + object.target.scaleY + "," + object.target.left + "," + object.target.top + "," + object.target.angle + ")";
-	  }
-	}.bind(this));
-}
-
-FabricHelper.prototype.display = function(text, id) {
+FabricHelper.prototype.setPosition = function() {
+	return "(" + this.activeObject.scaleX + "," + this.activeObject.scaleY + "," + this.activeObject.left + "," + this.activeObject.top + "," + this.activeObject.angle + ")";
 }
